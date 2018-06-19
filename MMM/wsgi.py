f402ee7003b0f95612761a73c48018016440d1ab
Marcelo Hideaki Iwata Kito <hideaki09@hotmail.com>
2018-06-19 11:03:11 -0300

    Host server

---
+++ MMM/wsgi.py
@@ -0,0 +1,25 @@
+"""
+WSGI config for MMM project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MMM.settings")
+
+application = get_wsgi_application()
+
+path = os.path.expanduser('~/hiideaki')
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hiideaki.settings'
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
